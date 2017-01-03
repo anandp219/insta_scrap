213e75fa97f8ab76cf0b1591dd2ec182c5431194
Anand Pandey <anandp219@gmail.com>
2017-01-03 14:52:29 +0530

    final

--- index.js
+++ index.js
@@ -1,7 +1,47 @@
 var http = require("http");
+var url = require('url');
+var req = require('request')
+
 
 http.createServer(function(request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("It's alive!");
-  response.end();
+    if (request.url === '/favicon.ico') {
+        response.writeHead(200, {'Content-Type': 'image/x-icon'} );
+        response.end();
+        return;
+    }
+    var parts = url.parse(request.url, true);
+    var query = parts.query;
+    username = query["username"]
+    var isPrivate = true
+    if(username) {
+        console.log(username)
+        req('https://instagram.com/' +username+ "/" , function (error, resp, body) {
+            //Check for error
+            if(error){
+                return console.log('Error:', error);
+            }
+
+            //Check for right status code
+            if(resp.statusCode !== 200){
+                return console.log('Invalid Status Code Returned:', response.statusCode);
+            }
+
+            if(body.indexOf('"is_private": false') > -1) {
+                isPrivate = false
+                response.writeHead(200, {"Content-Type": "text/plain"});
+                response.write(isPrivate.toString());
+                response.end();
+            } else {
+                isPrivate = true
+                response.writeHead(200, {"Content-Type": "text/plain"});
+                response.write(isPrivate.toString());
+                response.end();
+            }
+
+        });
+    } else {
+        response.writeHead(200, {"Content-Type": "text/plain"});
+        response.write("No username");
+        response.end();
+    }
 }).listen(process.env.PORT || 5000);
